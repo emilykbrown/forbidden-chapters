4ab9ee87c2bac50d186c24cffea64b0cf53b7391
emilykbrown <disabledsarcasm@gmail.com>
2024-05-05 18:11:12 -0500

    updated files

--- config/db.php
+++ config/db.php
@@ -17,31 +17,7 @@ try
         }
 
         return $randomString;
-<<<<<<< HEAD
     }
-=======
-   }
-=======
-    // echo uniqid();
-
-	// function create_unique_id() {
-    //     // $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    //     // $characterlength = strlen($characters);
-    //     // $randomString = '';
-    //     // for ($i=0; $i < 20; $i++) {
-    //     //     $randomString .= $characters[mt_rand(0, $characterlength -1)];
-    //     // }
-
-    //     // return $randomString;
-
-    //         return uniqid();
-        
-        
-
-    
-    
->>>>>>> 17ddd3bd128dcc8f14c3b146569e56b852811057
->>>>>>> a74aa313e402ec9223ae8df2aa90c76e1807425c
 }
 
 catch(PDOException $e)
