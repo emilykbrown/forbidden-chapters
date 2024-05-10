aa94e96e20a7d451e249a72ee2f8194d1831c345
emilykbrown <disabledsarcasm@gmail.com>
2024-05-10 01:02:45 -0500

    book card- card image no full width

---
+++ components/book_card.php
@@ -0,0 +1,34 @@
+<section class="py-5">
+    <div class="container px-4 px-lg-5 mt-5">
+        <div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+            <div class="col mb-5">
+                <div class="card h-100">
+                    <div class="img-container">
+                        <img class="book-img" src="https://dummyimage.com/180x225/dee2e6/6c757d.jpg" alt="..." />
+                        <div class="btn-overlay">
+                            <button class="btn btn-outline-dark mt-auto">Details</button>
+                        </div>
+                    </div>
+                    <div class="card-body p-4">
+                        <div class="text-center">
+                            <!-- Product name-->
+                            <h5">Catching Fire</h5>
+                            <p>Suzanne Collins</p>
+                            <p>$7.95</p>
+                        </div>
+                    </div>
+                    <!-- Product actions-->
+                    <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                        <div class="row">
+                            <div class="col text-center"><a class="btn btn-outline-dark mt-auto" href="#">Buy Now</a>
+                            </div>
+                            <div class="col text-center"><a class="btn btn-outline-dark mt-auto" href="#"><i
+                                        class="fa fa-solid fa-cart-shopping"></i></a></div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
   <?php
@@ -9,25 +8,15 @@
   <?php
     include 'components/navbar.php';
   ?>
-  <div class="col">
-    <div class="container mt-20">
-      <div class="book-container">
-        
-        
-        </div>
-        <div class="img-contaier">
-          <img src="test-img/catchingfire.jpg" alt="" width="150px" height="220px">
-        </div>
-        <div class="card-body">
-          <p class="book-card__title">Catching Fire</p>
-          <p class="book-card_author">Suzanne Collins</p>
-          <p class="book-card_genre">Young Adult</p>
-          <p class="book-card_price">$7.95</p>
-          <button>Add to cart</button>
-          <button>View details</button>
-        </div>
-      </div>
-    </div>
+    
+  </body>
+  </html>
+  
+  
+  <?php
+    include 'components/book_card.php';
+  ?>
+    
     
   </body>
   </html>
